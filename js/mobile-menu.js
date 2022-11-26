75df9e11d25aa60e2ded65742cd6b373b05d7e18
ZaharChis <Zahargoole@gmail.com>
2022-11-26 10:18:03 +0200

    1

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -21,3 +21,7 @@
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
     if (!e.matches) return;
     mobileMenu.classList.remove('is-open');
+    openMenuBtn.setAttribute('aria-expanded', false);
+    bodyScrollLock.enableBodyScroll(document.body);
+  });
+})();
